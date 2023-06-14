a8b02abc6832171dc48b5482617b1fd0228b183a
NageshSingh-VidyaMantra <124775387+NageshSingh-VidyaMantra@users.noreply.github.com>
2023-06-14 17:36:29 +0530

    unit test defination added

--- src/test/1 Suite/1_suites.test.js
+++ src/test/1 Suite/1_suites.test.js
@@ -1,25 +1,42 @@
 import { describe, test } from 'vitest'
 
-// To create Suite use the describe function,
-// which takes two argument
-// describe( " Suite Name/ description ", "callback function implementaion")
+/*
+  The fundamental idea of unit testing is that you test only one piece of functionality
+  in your application that usually is implemented by one function.
+
+  And you test it in isolation – 
+  meaning that all other parts of your application which 
+  that function depends on are not used by your tests.
+  Instead, they are imitated by your tests.
+*/
+
+
+/*
+  We create Suite by using the describe function, where we write our test case to test our function
+  It take two argument
+  descqribe( " Suite Name/ description ", "callback function implementaion")
+*/
 
 // suite - 1
 describe('Testing My Custom Function from File 1', ()=>{
 
     /** Multiple test can be written in a suite */
 
-    // TEST - 1
-    // like describe, test also take two arguments
-    // test( " Test Name/ description ", "callback function implementaion")
+    /* 
+       TEST - 1
+       like describe, test also take two arguments
+       test( " Test Name/ description ", "callback function implementaion")
+     */
     test('My first test case', ()=>{
         
-        // Here we do the unit testing by matching,
-        // Expected value : value we provide to the test.
-        // Received value : value the test get after the expression or function is resolved.
-        // at the end check is Received value = Expected value.
-        // if YES, test Pass
-        // else test Fail 
+      /*
+        Here we do the unit testing by matching,
+        Expected value : value we provide to the test.
+        Received value : value the test get after the expression or function is resolved.
+        at the end check is Received value = Expected value.
+        if YES, test Pass
+        else test Fail 
+      */
     
     })
 
